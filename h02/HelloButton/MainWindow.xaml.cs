9f8a54239a9148c6f78319fa9e060bd5442697bb
wouterPardon <wouter.pardon@live.be>
2018-02-07 08:46:48 +0100

    Oefeningen h2 toegevoegd

---
+++ h02/HelloButton/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace HelloButton
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextBox test = new TextBox();
+
+        public MainWindow()
+        {
+            
+            InitializeComponent();
+        }
+
+        private void klikHierButton_Click(object sender, RoutedEventArgs e)
+        {
+            test = (TextBox)this.FindName("TestBoxTextBox");
+            test.Visibility = Visibility.Hidden;
+            TestBoxTextBox.Text = "Dit is een test";
+            HelloLabel.Content = "Hallo";
+        }
+
+        private void HelloTextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
